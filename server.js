380dfedbe4c86c905e7b8bad788762ed7d81e970
samaxeola <59022695+samaxeola@users.noreply.github.com>
2019-12-19 06:23:03 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://music-v0-5-2.glitch.me/`);
+}, 280000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "_";
+const ownerID = "359802930724995073";
+const active = new Map();
+
+client.on('message', message => {
+
+
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    try {
+
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+      
+        let ops = {
+            ownerID: ownerID,
+            active: active
+        }
+
+        let commandFile = require(`./commands/${cmd}`);
+        commandFile.run(client, message, args, ops);
+
+    } catch (e) {
+        console.log(e);
+    }
+
+});
+
+client.on('ready', () => console.log('Im Ready Man :)'));
+
+client.login("NjA1NTA1MDQzNDY2Mjg5MTcy.XfJqWQ.0thDVhwxeOFWVN8OIfUCqY-53HQ");
